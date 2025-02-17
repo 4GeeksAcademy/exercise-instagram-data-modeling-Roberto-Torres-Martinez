f73860e2d5594ec069e3394d6d095af222f14596
Roberto Torres Martínez <roberxiri@gmail.com>
2025-02-17 12:47:07 +0000

    Done

--- src/models.py
+++ src/models.py
@@ -1,31 +1,58 @@
 import os
 import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, declarative_base, Mapped, mapped_column
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from typing import Literal
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+class Follower(Base):
+    __tablename__ = 'follower'
+    follower = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    get_id1 = Column(Integer, ForeignKey('post.id'))
+    get_id2 = Column(Integer, ForeignKey('comment.id'))
+    follower = relationship('follower', backref= 'user')
+    comment = relationship('comment', backref= 'user')
+    post = relationship('follower', backref= 'user')
 
-    def to_dict(self):
-        return {}
+FyleType = Literal['PHOTO', 'VIDEO', 'AUDIO']
 
-## Draw from SQLAlchemy base
+
+class Media(Base):
+    __tablename__ = 'media'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    type: Mapped[FyleType] = mapped_column(String, nullable=False)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    media = relationship('media', backref= 'post')
+    comment = relationship('comment', backref= 'post')
+
+class Comment(Base):
+     __tablename__ = 'comment'
+     id = Column(Integer, primary_key=True)
+     comment_text = Column(String(250), nullable=False)
+     post_id = Column(Integer, ForeignKey('post.id'))
+# Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
